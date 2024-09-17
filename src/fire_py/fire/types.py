4ff33f92c2ed116b31acfbd8d4ec9401b6fa4725
Jim Lundin <jimeriklundin@gmail.com>
2024-09-17 20:17:28 +0200

    misc

--- src/fire_py/fire/types.py
+++ src/fire_py/fire/types.py
@@ -2,69 +2,88 @@
 
 from __future__ import annotations
 
-from typing import Literal, TypedDict
+from typing import Literal, Required, TypedDict
 
 Reference = TypedDict("Reference", {"$ref": str})
 
 
-class BaseProperty(TypedDict, total=False):
+class Base(TypedDict, total=False):
+    """Base class for property types with an optional description."""
+
     description: str
 
 
-class StringProperty(BaseProperty):
-    type: Literal["string"]
-    enum: list[str]
+class Numeric[T](Base):
+    """Base class for numeric property types with minimum and maximum values."""
 
-
-class NumericProperty[T](BaseProperty):
-    type: Literal["number"]
     minimum: T
     maximum: T
     monetary: bool
 
 
-class IntegerProperty(NumericProperty[int]):
-    type: Literal["integer"]
+class Integer(Numeric[int]):
+    """Represents an integer property with optional monetary attribute."""
+
+    type: Required[Literal["integer"]]
     monetary: bool
 
 
-class NumberProperty(NumericProperty[float]):
-    type: Literal["number"]
+class Number(Numeric[float]):
+    """Represents a number property."""
+
+    type: Required[Literal["number"]]
 
 
-class BooleanProperty(BaseProperty):
-    type: Literal["boolean"]
+class Boolean(Base):
+    """Represents a boolean property."""
+
+    type: Required[Literal["boolean"]]
+
+class String(Base):
+    """Represents a string property with an optional enumeration of allowed values."""
+
+    type: Required[Literal["string"]]
+    enum: list[str]
 
 
-class DateProperty(BaseProperty):
-    type: Literal["string"]
-    format: Literal["date"]
+class Date(String):
+    """Represents a date property."""
+
+    format: Required[Literal["date"]]
 
 
-class DateTimeProperty(BaseProperty):
-    type: Literal["string"]
-    format: Literal["date-time"]
+class DateTime(String):
+    """Represents a date-time property."""
+
+    format: Required[Literal["date-time"]]
 
 
-class ArrayProperty(BaseProperty):
-    type: Literal["array"]
-    items: list[Property]
+class Array(Base):
+    """Represents an array property with a list of items."""
+
+    type: Required[Literal["array"]]
+    items: Property | Reference
     minItems: int
+    uniqueItems: bool
 
 
-class ObjectProperty(BaseProperty):
-    type: Literal["object"]
-    properties: dict[str, Property]
+
+class Object(Base):
+    """Represents an object property with properties and required fields."""
+
+    title: str
+    type: Required[Literal["object"]]
+    properties: dict[str, Property | Reference]
     required: list[str]
     additionalProperties: bool
 
 
 Property = (
-    StringProperty
-    | IntegerProperty
-    | NumberProperty
-    | BooleanProperty
-    | DateTimeProperty
-    | ArrayProperty
-    | ObjectProperty
+    String
+    | Integer
+    | Number
+    | Boolean
+    | DateTime
+    | Array
+    | Object
 )
